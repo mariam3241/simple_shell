3b4a3cdd473ff4a16da367b35b45638c2fc91d26
khadigayahya <khadigayahya84@gmail.com>
2023-07-19 16:33:04 -0700

    commit

---
+++ string_functions.c
@@ -0,0 +1,74 @@
+/**
+ * _strlen - returns the length of a string
+ *
+ * @s: string parameter input
+ *
+ * Return: length of string
+*/
+
+int _strlen(char *s)
+{
+	int counter;
+
+	for (counter = 0; *s != '\0'; s++)
+		++counter;
+
+	return (counter);
+}
+
+/**
+ * _strcmp - compares two strings
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * Return: negative if s1 < s2,positive if s1 > s2, zero if s1 == s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
+	}
+	if (*s1 == *s2)
+		return (0);
+	else
+		return (*s1 < *s2 ? -1 : 1);
+}
+
+/**
+ * starts_with - checks if needle starts with haystack
+ * @haystack: string to search
+ * @needle: the sudstring to find
+ *
+ * Return:address of next char or NULL
+ */
+char *starts_with(const char *haystack, const char *needle)
+{
+	while (*needle)
+		if (*needle++ != haystack++)
+			return (NULL);
+	return ((char *)haystack);
+}
+
+/**
+ * _strcat - function that concatenates two strings
+ *
+ * @dest: pointer to destnation input
+ * @src: pointer to source input
+ *
+ * Return: pointer to resulting string @dest
+*/
+char *_strcat(char *dest, char *src)
+{
+	char *ret = dest;
+
+	while (*dest)
+		dest++;
+	while (*src)
+		*dest++ = *src++;
+	*dest = *src;
+	return (ret);
+}
---
+++ string_functions3.c
@@ -0,0 +1,74 @@
+#include "shell.h"
+
+/**
+ * _strncpy - A function that copies a string.
+ * @dest: pointer to destination input buffer
+ * @src: pointer to source input buffer
+ * @n: bytes of @src
+ * Return: @dest
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	while (src[i] != '\0' && i < n - 1)
+	{
+		dest[i] = src[i]
+		i++;
+	}
+	if (i < n)
+	{
+		j = i;
+		while (j < n)
+		{
+			dest[i] = '\0';
+			j++;
+		}
+	}
+	return (s);
+}
+
+/**
+ * _strncat - a function that concatenates two strings.
+ * @dest: pointer to destination input
+ * @src: pointer to source input
+ * @n: most number of bytes from @src
+ * Return: @dest
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int c, i;
+	char *s = dest;
+
+	i = 0;
+	c = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[c] != '\0' && c < n)
+	{
+		dest[i] = src[c];
+		i++;
+		c++;
+	}
+	if (c < n)
+		dest[i] = '\0';
+	return (s);
+}
+
+/**
+ * *_strchr - fills memory with a constant byte.
+ * @s: pointer to put the constant
+ * @c: constant
+ * Return: pointer to s
+ */
+char *_strchr(char *s, char c)
+{
+	do {
+		if (*s == c)
+			return (s);
+	} while (*s++ != '\0');
+
+	return (NULL);
+}
